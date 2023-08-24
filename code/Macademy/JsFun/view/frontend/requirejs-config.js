f70cba77f682a8b807be8e4f7e3a5f0079de8620
Gal <galg@elad.co.il>
2023-08-24 15:10:41 +0300

    JsFun : override js file

--- code/Macademy/JsFun/view/frontend/requirejs-config.js
+++ code/Macademy/JsFun/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     "map":{
         "*":{
-            "fadeInElement":"Macademy_JsFun/js/fade-in-element"
+            "fadeInElement":"Macademy_JsFun/js/fade-in-element",
+            "Magento_Review/js/submit-review":"Macademy_JsFun/js/submit-review"
         }
     },
     "paths":{
