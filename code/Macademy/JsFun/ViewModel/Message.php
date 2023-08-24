77e07d2c41e9f3f1729ead1b0533f3c73b597a3f
Gal <galg@elad.co.il>
2023-08-24 15:03:27 +0300

    JsFun : create message viewModel

---
+++ code/Macademy/JsFun/ViewModel/Message.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Macademy\JsFun\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class Message implements ArgumentInterface
+{
+
+    public function getMessage()
+    {
+        return str_shuffle('Declerative Text');
+    }
+}
