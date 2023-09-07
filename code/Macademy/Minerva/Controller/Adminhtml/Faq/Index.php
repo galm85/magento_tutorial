60af602b7fa3ddff8f02256dde8f17a16db985eb
Gal <galg@elad.co.il>
2023-09-07 15:05:35 +0300

    AdminGrid : create faq controller and route

---
+++ code/Macademy/Minerva/Controller/Adminhtml/Faq/Index.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Macademy\Minerva\Controller\Adminhtml\Faq;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+
+    const ADMIN_RESOURCE = 'Macademy_Minerva::faq';
+
+    /** @var PageFactory */
+    protected $pageFactory;
+
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        return $this->pageFactory->create();
+    }
+}
